8b074bd90aa20249512a7ce5cb95b9a3f3250672
droneal11015 <droneal11015@gmail.com>
2019-04-04 16:01:40 -0400

    created first class in lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,14 @@
 // CODE here for your Lambda Classes
+
+
+class Person {
+    constructor(attributes){
+      this.newAge = attributes.age;
+      this.newName = attributes.name;
+      this.newLocation = attributes.location;
+      this.newGender = attributes.gender;
+    }
+    speak() {
+        return `Hello, my name is ${this.newName}, I am from ${this.newLocation}`;
+      }
+    }
